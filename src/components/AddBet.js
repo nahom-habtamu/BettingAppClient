bc2124dd191a2b535b330d7911b5d0cc6a5ab85b
Nahom Habtamu <nahom-habtamu>
2020-12-27 22:13:00 +0300

    add bet on progress

--- src/components/AddBet.js
+++ src/components/AddBet.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MainTop from './MainTop';
 import { Link } from 'react-router-dom';
-import { IoPersonAddSharp } from 'react-icons/io';
+import { BsFillPersonPlusFill } from 'react-icons/bs';
 import { FaShareAlt } from 'react-icons/fa';
 
 export default class AddBet extends Component {
@@ -18,25 +18,40 @@ export default class AddBet extends Component {
                     </div>
                     <div className="main-section">
                         <div className="row">
-                            <div className="col-xl-5 col-lg-6 col-md-6 col-sm-12">
+                            <div className="col-xl-5 col-lg-6 col-md-8 col-sm-12">
                                 <div className="main-box">
-                                    <div className="box-labels">
+                                    <div className=" box-labels">
                                         <label className="select-date">Select Date</label>
                                         <label className="select-time">Select time</label>
                                     </div>
-                                    <div className="box-inputs">
-                                        <input type="date"/>
-                                        <input type="time"/>
-                                    </div>
+                                    <form>
+                                        <div className="date-time-inputs">
+                                            <input type="date"/>
+                                            <input type="time"/>
+                                        </div>
+                                        <div className="container currency-wrapper">
+                                            <select className="currency">
+                                                <option>Pound</option>
+                                                <option>Dollar</option>
+                                                <option>Yuan</option>
+                                                <option>US Dollar</option>
+                                                <option>Canadian Dollar</option>
+                                            </select>
+                                        </div>
+                                    </form>
                                 </div>
-                            </div>
-                            <div className="col-xl-7 col-lg-6 col-md-6 col-sm-12">
+                            </div><br/>
+                            <div className="col-xl-7 col-lg-6 col-md-4 col-sm-12 add-share">
                                 <Link to="/notification" className="icon-button">
-                                    <IoPersonAddSharp size="1.5em" color="bluegreen"/>
+                                    <BsFillPersonPlusFill size="1.5em" color="bluegreen"/>
                                 </Link>
                                 <Link to="/profile" className="icon-button">
                                     <FaShareAlt size="1.5em" color="bluegreen"/>
                                 </Link>
+                                <div className="button-helpers">
+                                    <label>Invite users</label>
+                                    <label>share link</label>
+                                </div>
                             </div>
                         </div>
                     </div>
